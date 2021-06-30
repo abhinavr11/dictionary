3af391e32451e37ec3238ffb9c60a25d6161b77c
Abhinav Raghuvanshi <78714364+abhinavr11@users.noreply.github.com>
2021-06-30 12:31:09 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,15 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+console.log('background running');
+
+//chrome.runtime.onMessage.addListener(receiver);
+
+//window.word = 'coding train';
+
+//function receiver(request, sender, sendResponse) {
+ // console.log(request);
+  //window.word = request.text;
+  //sendResponse();
+//}
